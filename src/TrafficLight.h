7ba0a10728a57e790acd09630e4815eab273b2ce
Joao Gouveia <j.lucas.gouveia@gmail.com>
2021-08-12 10:13:43 +0100

    optimize constants

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -18,8 +18,8 @@ private:
     void InvertLight();
     TrafficLightPhase _currentPhase { TrafficLightPhase::red }; /* arbitrary choose */
     int _currentCycleTimeSec;
-    const int CYCLE_TIME_TOP_LIMIT = 6;
-    const int CYCLE_TIME_FLOOR_LIMIT = 4;
+    static constexpr int CYCLE_TIME_TOP_LIMIT = 6; //no need to define this in runtime, put the constexpr to define in compile time
+    static constexpr int CYCLE_TIME_FLOOR_LIMIT = 4; //no need to define this in runtime, put the constexpr to define in compile time
 
 public:
     TrafficLight();
